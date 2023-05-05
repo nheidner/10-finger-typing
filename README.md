0fca5aa08b04440680270fadcece6aa404e2a89b
Nikolas <niko.heidner@gmail.com>
2023-05-05 22:35:33 +0200

    feat: add docker compose dev and build profiles

---
+++ README.md
@@ -0,0 +1,4 @@
+# 10 finger typing
+
+`docker compose --profile dev up --build`: start dev environment
+`docker compose --profile build up --build`: create and run build
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://server:8080/api/:path*'
+        destination: `${process.env.API_URL}/:path*`
       }
     ]
   },
