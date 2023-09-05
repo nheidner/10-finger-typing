214d9f269eada6141204f3a7ccc7d909fa83eeef
Nikolas <niko.heidner@gmail.com>
2023-09-05 09:56:00 +0200

    feat: add cleaning script

---
+++ server/scripts/clean/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"10-typing/models"
+	"os"
+)
+
+func main() {
+	models.ConnectDatabase()
+
+	userService := models.UserService{
+		DB: models.DB,
+	}
+	sessionService := models.SessionService{
+		DB: models.DB,
+	}
+	scoreService := models.ScoreService{
+		DB: models.DB,
+	}
+	textService := models.TextService{
+		DB: models.DB,
+	}
+	roomService := models.RoomService{
+		DB: models.DB,
+	}
+
+	err := userService.DeleteAll()
+	if err != nil {
+		os.Exit(1)
+		return
+	}
+
+	err = sessionService.DeleteAll()
+	if err != nil {
+		os.Exit(1)
+		return
+	}
+
+	err = scoreService.DeleteAll()
+	if err != nil {
+		os.Exit(1)
+		return
+	}
+
+	err = textService.DeleteAll()
+	if err != nil {
+		os.Exit(1)
+		return
+	}
+
+	err = roomService.DeleteAll()
+	if err != nil {
+		os.Exit(1)
+	}
+}
