b19b7449e8c7e173321b3012d133d7e44eecd66f
Nikolas <niko.heidner@gmail.com>
2023-10-04 18:11:58 +0200

    feat: show loading indication for scores

--- frontend/src/modules/profile/components/Scores.tsx
+++ frontend/src/modules/profile/components/Scores.tsx
@@ -1,31 +1,19 @@
 import { getScoresByUsername } from "@/utils/queries";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { SortByOption } from "../types";
 import { SortBy } from "./SortBy";
 import { Score } from "@/types";
+import classNames from "classnames";
 
 export const ScoresList = ({ data }: { data?: Score[] }) => {
   if (!data) {
-    return (
-      <tbody>
-        <tr>
-          <td colSpan={4}>
-            <div
-              className="flex justify-center items-center w-full"
-              style={{ height: "522.5px" }}
-            >
-              Loading ...
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    );
+    return null;
   }
 
   return (
     <tbody className="bg-white">
-      {data.slice(0, 10).map((score) => {
+      {data.map((score) => {
         const wordsPerMinute = score.wordsPerMinute.toFixed(1);
         const accuracy = `${score.accuracy.toFixed(2)} %`;
         const numberErrors = score.numberErrors;
@@ -53,15 +41,31 @@ export const ScoresList = ({ data }: { data?: Score[] }) => {
   );
 };
 
+const Overlay = ({ isLoading }: { isLoading: boolean }) => {
+  return (
+    <div
+      className={classNames(
+        "absolute left-0 top-0 w-full h-full bg-gray-300 transition-opacity z-10 duration-100",
+        isLoading ? "opacity-50" : "opacity-0"
+      )}
+    />
+  );
+};
+
 export const Scores = ({ username }: { username: string }) => {
   const [sortBy, setSortBy] = useState<SortByOption>("recent");
-
   const sortByParamValues = sortBy === "recent" ? [] : [`${sortBy}.desc`];
+
+  const dataRef = useRef<Score[] | undefined>(undefined);
 
   const { data, isLoading } = useQuery({
     queryKey: ["score", username, sortBy],
     queryFn: () => getScoresByUsername(username, { sortBy: sortByParamValues }),
   });
+
+  if (data) {
+    dataRef.current = data;
+  }
 
   return (
     <section>
@@ -76,35 +80,37 @@ export const Scores = ({ username }: { username: string }) => {
         </div>
         <SortBy sortBy={sortBy} setSortBy={setSortBy} />
       </div>
-      <table className="min-w-full divide-y divide-gray-300">
-        <thead>
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3 block"
-            >
-              Words Per Minute
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Accuracy
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Number of Errors
-            </th>
-            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
-        </thead>
-
-        <ScoresList data={data} />
-      </table>
+      <div className="relative">
+        <Overlay isLoading={isLoading} />
+        <table className="min-w-full divide-y divide-gray-300 table-fixed">
+          <thead className="z-20 bg-white relative">
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3 block"
+              >
+                Words Per Minute
+              </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+              >
+                Accuracy
+              </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+              >
+                Number of Errors
+              </th>
+              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <ScoresList data={dataRef.current} />
+        </table>
+      </div>
     </section>
   );
 };
