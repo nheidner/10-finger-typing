8e35a0520f0b5945d9af6108209858df39563be8
Nikolas <niko.heidner@gmail.com>
2023-10-20 16:08:43 +0200

    feat: add game service setNewCurrentGame method

--- server/models/game_create.go
+++ server/models/game_create.go
@@ -1,5 +1,38 @@
 package models
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"strconv"
 
-func (gs *GameService) Create(ctx context.Context) {}
+	"github.com/google/uuid"
+)
+
+// rooms:[roomId]:current_game:user_ids
+// rooms:[roomId]:current_game text_id status
+func (gs *GameService) SetNewCurrentGame(ctx context.Context, textId, roomId uuid.UUID, userIds ...uuid.UUID) error {
+	if len(userIds) == 0 {
+		return fmt.Errorf("at least one user id must be specified")
+	}
+
+	currentGameKey := getCurrentGameKey(roomId)
+	statusStr := strconv.Itoa(int(UnstartedGameStatus))
+	currentGameValue := map[string]string{
+		"text_id": textId.String(),
+		"status":  statusStr,
+	}
+	if err := gs.RDB.HSet(ctx, currentGameKey, currentGameValue).Err(); err != nil {
+		return err
+	}
+
+	currentGameUseridsKey := getCurrentGameUserIdsKey(roomId)
+	userIdStrs := make([]interface{}, 0, len(userIds))
+	for _, userId := range userIds {
+		userIdStrs = append(userIdStrs, userId.String())
+	}
+	if err := gs.RDB.SAdd(ctx, currentGameUseridsKey, userIdStrs...).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
